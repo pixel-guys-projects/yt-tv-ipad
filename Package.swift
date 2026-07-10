e767e1048f64da137e27cf34e071291908381e55
Pixel Guy <hanschannels429@gmail.com>
2026-07-10 17:12:21 +0800

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 5.9
+import PackageDescription
+import AppleProductTypes
+
+let package = Package(
+    name: "YouTubeTVApp",
+    platforms: [
+        .iOS(.v17)
+    ],
+    products: [
+        .iOSApplication(
+            name: "YouTubeTVApp",
+            targets: ["AppModule"],
+            bundleIdentifier: "com.pixelguys.yttvipad",
+            teamIdentifier: "",
+            displayVersion: "1.0",
+            bundleVersion: "1",
+            appIcon: .placeholder(icon: .airplane),
+            supportedDeviceFamilies: [
+                .pad,
+                .phone
+            ],
+            supportedInterfaceOrientations: [
+                .landscapeRight,
+                .landscapeLeft
+            ]
+        )
+    ],
+    targets: [
+        .executableTarget(
+            name: "AppModule",
+            path: "."
+        )
+    ]
+)
